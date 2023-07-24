e9ea55f3db1ca5916db4908a0fad84c9b301d899
Prantadas7 <pranta_das@coredevs.ltd>
2023-07-24 14:10:28 +0600

    FEAT: room entities added

--- src/entities/room.entitiy.js
+++ src/entities/room.entitiy.js
@@ -1,12 +1,17 @@
 const createAllowed = new Set(['users', 'roomStatus', 'roomId']);
 const { v4 } = require('uuid');
+const Room = require('../db/schemas/room.schema');
 
 const createRoom = async (req, res) => {
     try {
-        if (!users) return res.status(400).send({ message: 'Bad request' });
+        if (!req?.body?.users) return res.status(400).send({ message: 'Bad request' });
         const isValid = Object.keys(req.body).every((key) => createAllowed.has(key));
         if (isValid) return res.status(400).send({ message: 'Bad request' });
-
+        const roomId = v4();
+        req.body.roomId = roomId;
+        const room = new Room(req.body);
+        await room.save();
+        return res.status(200).send(room);
     }
 
     catch (err) {
@@ -14,3 +19,19 @@ const createRoom = async (req, res) => {
         return res.status(500).send({ message: err.message || 'Something went wrong' });
     }
 };
+
+const deleteRoom = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const room = await Room.findOne({ _id: id });
+        if (!room) return res.status(404).send({ message: 'Room not found' });
+        await room.deleteOne();
+        return res.status(200).send({ message: 'Room deleted' });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).send({ message: err.message || 'Something went wrong' });
+    }
+};
+
+module.exports = { createRoom, deleteRoom };
